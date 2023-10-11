45d16c7cb80832055a64a890b0b17d76c6b5881c
pomegranatesPome <naiyuwan@usc.edu>
2023-10-11 16:21:16 -0700

    Added mouse pointer; Made bullet blocked by walls except transparent ones; Made aiming and moving mutual exclusive

--- Assets/Scripts/EnemyScripts/EnemyController.cs
+++ Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -7,10 +7,6 @@ public class EnemyController : MonoBehaviour
 {
    private void Update()
    {
-      if (Input.GetKeyDown(KeyCode.Alpha2))
-      {
-         CharacterHealth ch = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<CharacterHealth>();
-         ch.curHealth -= 1;
-      }
+      
    }
 }
