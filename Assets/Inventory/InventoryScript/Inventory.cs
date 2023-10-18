8f4cefa2338b9609caa7c48dc2a2e9062a5688e0
lixinjack <lixinjack@gmail.com>
2023-10-17 17:14:37 -0700

    inventory update

--- Assets/Inventory/InventoryScript/Inventory.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory/New Inventory")]
-public class Inventory : ScriptableObject
-{
-    public List<Item> itemList = new List<Item>();
-}
