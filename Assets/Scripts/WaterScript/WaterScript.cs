3155f61d47819eee4a12e304a57107429531e3d9
Jonohan <wanghhjohn@outlook.com>
2023-10-26 21:33:45 -0700

    Tutorial level 1 and new guidance

--- Assets/Scripts/WaterScript/WaterScript.cs
+++ Assets/Scripts/WaterScript/WaterScript.cs
@@ -9,10 +9,17 @@ public class WaterScript : MonoBehaviour
     public GameObject manager = null;
     public Text enemyKillCountText;
 
+/*    [Header("Count setting")]
+    public int initialPatrolEnemyCount = 0;
+
+    private WaterAttackManager waterAttackManager;*/
+
     // Start is called before the first frame update
     void Start()
     {
-    
+/*        waterAttackManager = manager.GetComponent<WaterAttackManager>();
+
+        waterAttackManager.enemy = initialPatrolEnemyCount;*/
     }
 
     // Update is called once per frame
@@ -22,39 +29,71 @@ public class WaterScript : MonoBehaviour
     }
 
     // Checking collision with other game object
-    private void OnCollisionEnter(Collision collision)
+/*    private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collision with " + collision.gameObject.name);
+
         if (collision.gameObject.name.Contains("EnemyGB"))
         {
-            manager.GetComponent<WaterAttackManager>().enemy += 1;
+            waterAttackManager.enemy += 1;
 
             collision.gameObject.SetActive(false);
-            Debug.Log("Swallow EnemyGB amount:" + manager.GetComponent<WaterAttackManager>().enemy);
-            manager.GetComponent<WaterAttackManager>().recover();
+            Debug.Log("Swallow EnemyGB amount:" + waterAttackManager.enemy);
+            waterAttackManager.recover();
 
-            UpdateEnemyKillText(manager.GetComponent<WaterAttackManager>().enemy, "EnemyGB");
+            UpdateEnemyKillText(waterAttackManager.enemy, "EnemyGB");
         }
 
         if (collision.gameObject.name.Contains("PatrolEnemy"))
         {
-            manager.GetComponent<WaterAttackManager>().patrolEnemy += 1;
+            waterAttackManager.patrolEnemy += 1;
 
             collision.gameObject.SetActive(false);
-            Debug.Log("Swallow patrolEnemy amount:" + manager.GetComponent<WaterAttackManager>().patrolEnemy);
-            manager.GetComponent<WaterAttackManager>().recover();
+            Debug.Log("Swallow patrolEnemy amount:" + waterAttackManager.patrolEnemy);
+            waterAttackManager.recover();
 
-            UpdateEnemyKillText(manager.GetComponent<WaterAttackManager>().patrolEnemy, "PatrolEnemy");
+            UpdateEnemyKillText(waterAttackManager.patrolEnemy, "PatrolEnemy");
         }
-        //// Check if the collision involves a specific tag
-        Debug.Log("Collision with " + collision.gameObject.name);
 
-        if (manager.GetComponent<WaterAttackManager>().patrolEnemy >=3 || manager.GetComponent<WaterAttackManager>().patrolEnemy >= 3)
+        if (waterAttackManager.patrolEnemy >= 3 || waterAttackManager.enemy >= 3)
         {
-            manager.GetComponent<WaterAttackManager>().canEnterFightingState = true;
+            waterAttackManager.canEnterFightingState = true;
             Debug.Log("You have collected 3 same type bodies, press 3 to Imitate them.");
         }
-    }
+    }*/
+      private void OnCollisionEnter(Collision collision)
+       {
+           Debug.Log("Collision with " + collision.gameObject.name);
+           if (collision.gameObject.name.Contains("EnemyGB"))
+           {
+               manager.GetComponent<WaterAttackManager>().enemy += 1;
+
+               collision.gameObject.SetActive(false);
+               Debug.Log("Swallow EnemyGB amount:" + manager.GetComponent<WaterAttackManager>().enemy);
+               manager.GetComponent<WaterAttackManager>().recover();
+
+               UpdateEnemyKillText(manager.GetComponent<WaterAttackManager>().enemy, "EnemyGB");
+           }
+
+           if (collision.gameObject.name.Contains("PatrolEnemy"))
+           {
+               manager.GetComponent<WaterAttackManager>().patrolEnemy += 1;
+
+               collision.gameObject.SetActive(false);
+               Debug.Log("Swallow patrolEnemy amount:" + manager.GetComponent<WaterAttackManager>().patrolEnemy);
+               manager.GetComponent<WaterAttackManager>().recover();
+
+               UpdateEnemyKillText(manager.GetComponent<WaterAttackManager>().patrolEnemy, "PatrolEnemy");
+           }
+           //// Check if the collision involves a specific tag
+           Debug.Log("Collision with " + collision.gameObject.name);
+
+           if (manager.GetComponent<WaterAttackManager>().patrolEnemy >=3 || manager.GetComponent<WaterAttackManager>().patrolEnemy >= 3)
+           {
+               manager.GetComponent<WaterAttackManager>().canEnterFightingState = true;
+               Debug.Log("You have collected 3 same type bodies, press 3 to Imitate them.");
+           }
+       }
 
     // Need to show different type enemy at the same screen in next version
     private void UpdateEnemyKillText(int count, string enemyType)
